cab240254a1fe080ee59017634b0ee6ee8b49e38
AuyelbekMadiyar <145276633+AuyelbekMadiyar@users.noreply.github.com>
2023-09-17 21:19:16 +0600

    lab#1

---
+++ lab1.py
@@ -0,0 +1,37 @@
+#сумма трех чисел
+a = int(input())
+b = int(input())
+c = int(input())
+sum = a + b + c
+print(sum)
+#Площадь прямоугольного тр
+b = int(input())
+h = int(input())
+area = 0.5 * b * h
+print(area)
+#Дележ яблок
+n = int(input())
+k = int(input())
+apples_student = k // n
+apples_basket = k% n
+print(apples_student)
+print(apples_basket)
+#Электронные часы
+n = int(input())
+hour = (n // 60) % 24
+minute = n % 60
+print(hour,minute)
+#Hello, Harry!
+name = input()
+print("Hello, " + name + "!")
+#Следующее и предыдущее
+num = int(input())
+next_num = num + 1
+prev_num = num - 1
+print(f'The next number for the number {num} is {next_num}.')
+print(f'The previous number for the number {num} is {prev_num}.')
+
+
+
+
+
