beafbf2c9566d6ffb55181efdfcc4df53a05e49b
AuyelbekMadiyar <145276633+AuyelbekMadiyar@users.noreply.github.com>
2023-09-25 05:43:20 +0600

    lab2(Auyelbek Madiyar)

---
+++ 13assign.py
@@ -0,0 +1,7 @@
+n = int(input())
+m = int(input())
+x = int(input())
+y = int(input())
+min_dist = min(x, y, n - x, m - y)
+print(min_dist)
+
---
+++ 4assign.py
@@ -0,0 +1,5 @@
+x = int(input())
+y = int(input())
+z = int(input())
+
+print(min(x,y,z))
